27858986c9d851a6864bdccdb494a1a6cd80422a
Dan Jones <dan_j@live.co.uk>
2016-04-20 23:33:28 +0100

    Login added

--- scripts/forumscripts/login.php
+++ scripts/forumscripts/login.php
@@ -31,27 +31,66 @@ class login{
     function login($password){
         $this->password = htmlentities($this->link->real_escape_string($password));
 
-
         $query = "SELECT password FROM users WHERE username = '$this->username'";
-
-
 
         $result = $this->link->query($query) or die($this->link->error);
 
         $correctpassword = $result->fetch_array()[0];
 
-        echo $correctpassword;
+        $correcthash = $this->shuffle(md5($correctpassword));
+
+        echo md5($correctpassword);
+        echo ":";
+        echo $this->antihash($correcthash);
+
+        if($password == $correcthash){
+            return true;
+        }
+        return false;
+    }
+
+    function shuffle($correctpassword){
+        $password = $correctpassword;
+
+
+        $password = strrev($password); //reverse the md5
+
+        $password = substr($password,16,16).substr($password,0,16);
+
+        $password = (hexdec(substr($password,24,8))+10101).",".
+            (hexdec(substr($password,16,8))+10101).",".
+            (hexdec(substr($password,8,8))+10101).",".
+            (hexdec(substr($password,0,8))+34);
+
+        $password = strrev($password);
+
+        return $password;
 
     }
 
-    function myhash($correctpassword){
+    function antihash($password){
+
+        $password = strrev($password);
+
+        $password = explode(",",$password);
+
+
+        $password = dechex($password[3]-34).dechex($password[2]-10101).dechex($password[1]-10101).dechex($password[0]-10101);
+        
+        $password = substr($password,16,16).substr($password,0,16);
+        
+        $password = strrev($password);
+        return $password;
 
     }
 
 }
 
-$login = new login("dan_j@live.co.uk");
-$login->login("hiya");
-
-
+$login = new login("dan_j@live.co.ukasdasdfs");
+for($i = 0; $i<64; $i++)
+{
+    $login->nonce();
+    echo "<br>";
+    $login->login("hiya");
+}
 ?>
