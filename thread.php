bfc600adf9242c031340b92e0a8e334c5a41349d
Dan Jones <dan_j@live.co.uk>
2016-04-21 18:52:13 +0100

    Forum is completed

--- thread.php
+++ thread.php
@@ -10,7 +10,7 @@ if(isset($_GET['new'])){
         if($_POST['name'] == "")
         {
             $posterror = "Thread name cannot be blank";
-            new_post($posterror);
+            new_thread($posterror);
             die();
         }
         $thread = new thread();
@@ -21,12 +21,50 @@ if(isset($_GET['new'])){
         die();
 
     }
-    new_post($posterror);
+    new_thread($posterror);
     die();
 }
 
-function new_post($posterror){
+if(isset($_GET['thread']) && isset($_POST['post'])){
+    if($session->valid==1){
+        $post = new post();
 
+        $post->new_post($session->get_user_id(),$_GET['thread'],$_POST['post']);
+    }
+}
+
+
+$thread = new thread();
+
+$thread->get_thread($_GET['thread']);
+if($thread->threadname == ""){
+    echo "<br>Thread not found<br>";
+    die();
+}
+echo "<br>Thread: ".$thread->threadname."<br>";
+
+$post = new post();
+$postids = $thread->list_posts();
+foreach($postids as $postid){
+    $post->get_post($postid);
+    echo "<br>".get_user_from_id($post->userid)." - ".$post->date;
+    echo "<br>".$post->content;
+
+}
+
+if($session->valid==1){
+    echo "<br>";
+?>
+<form action="thread.php?thread=<?php echo $_GET['thread']; ?>" method="post">
+<textarea name="post" rows=10 cols=40></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+}
+
+
+function new_thread($posterror){
 ?>
 <br><br>
 <form action="thread.php?new=<?php echo $_GET['new']; ?>" method="post">
